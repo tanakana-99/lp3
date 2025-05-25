3806a3c661fab9980a1686e427326db7ba3e82b6
tanakana-99 <kanakon527@gmail.com>
2025-05-25 23:14:16 +0900

    SPのPPチェック後のコミット

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,16 +1,45 @@
+// カスタムドットのアクティブ状態を切り替える関数
+function updateCustomDots(index) {
+  $('.custom-dot').removeClass('active');
+  $('.custom-dot').eq(index).addClass('active');
+}
+
+// Slick 初期化前に init イベントで初期位置を2枚目に設定
+$('.voices__box').on('init', function () {
+  // 少し遅延させるとより安定します
+  setTimeout(function () {
+    $('.voices__box').slick('slickGoTo', 1);
+    updateCustomDots(1);
+  }, 50);
+});
+
+// Slick 初期化
 $('.voices__box').slick({
-  infinite: true,
-  dots: true,
+  infinite: false,
+  slidesToShow: 3,
+  slidesToScroll: 1,
   arrows: false,
-  variableWidth: false,
+  dots: false, // デフォルトドット無効
+  centerMode: false, // 中央寄せしない
   responsive: [{
     breakpoint: 768,
     settings: {
       slidesToShow: 1,
       slidesToScroll: 1,
-      variableWidth: false,
       centerMode: true,
-      centerPadding: "14%",
+      variableWidth: true
     }
   }]
+});
+
+// カスタムドットをクリックしたときの処理
+$('.custom-dot').on('click', function () {
+  const index = $(this).data('index');
+  $('.voices__box').slick('slickGoTo', index);
+  updateCustomDots(index);
+});
+
+// スライドが切り替わった後にドットのアクティブ状態を更新
+$('.voices__box').on('afterChange', function (event, slick, currentSlide) {
+  updateCustomDots(currentSlide);
 });
