f83f183ac16ff216af4b0947d32820aa17c340f0
tanakana-99 <kanakon527@gmail.com>
2025-05-28 23:31:08 +0900

    初校提出前のコミット

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,26 +1,26 @@
-// カスタムドットのアクティブ状態を切り替える関数
+
 function updateCustomDots(index) {
   $('.custom-dot').removeClass('active');
   $('.custom-dot').eq(index).addClass('active');
 }
 
-// Slick 初期化前に init イベントで初期位置を2枚目に設定
+
 $('.voices__box').on('init', function () {
-  // 少し遅延させるとより安定します
   setTimeout(function () {
     $('.voices__box').slick('slickGoTo', 1);
     updateCustomDots(1);
   }, 50);
 });
 
-// Slick 初期化
+
 $('.voices__box').slick({
   infinite: false,
   slidesToShow: 3,
   slidesToScroll: 1,
+  adaptiveHeight: false,
   arrows: false,
-  dots: false, // デフォルトドット無効
-  centerMode: false, // 中央寄せしない
+  dots: false,
+  centerMode: false,
   responsive: [{
     breakpoint: 768,
     settings: {
@@ -32,23 +32,16 @@ $('.voices__box').slick({
   }]
 });
 
-// カスタムドットをクリックしたときの処理
+
 $('.custom-dot').on('click', function () {
   const index = $(this).data('index');
   $('.voices__box').slick('slickGoTo', index);
   updateCustomDots(index);
 });
 
-// スライドが切り替わった後にドットのアクティブ状態を更新
+
 $('.voices__box').on('afterChange', function (event, slick, currentSlide) {
   updateCustomDots(currentSlide);
 });
 
 
-$(function() {
-  $('.course__description-02').each(function() {
-    var html = $(this).html();
-    html = html.replace(/、/g, '<span class="break">、</span>');
-    $(this).html(html);
-  });
-});
